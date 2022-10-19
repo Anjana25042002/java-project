7f1a33ac2b383f907313371e030a855f13ddb01f
ELCOT <ELCOT@Lenovo>
2022-10-19 16:13:53 +0530

    second commit

---
+++ Relational operators/src/equal_to.java
@@ -0,0 +1,13 @@
+
+public class equal_to {
+
+	public static void main(String[] args) {
+	int a = 5 , b = 10 , c = 5;
+	System.out.println("a = " + a);
+	System.out.println("b = " + b);
+	System.out.println("c = " + c);
+	System.out.println("a == b: "+(a==b));
+	System.out.println("a == c: "+(a==c));
+	}
+
+}
