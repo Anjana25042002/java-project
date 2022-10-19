b1f5ecc75ac3b32b3317c8cbf1e62bcb2221e567
ELCOT <ELCOT@Lenovo>
2022-10-19 16:22:21 +0530

    second commit

---
+++ Logical operator/src/AND.java
@@ -0,0 +1,15 @@
+
+public class AND {
+
+	public static void main(String[] args) {
+		int a = 20 , b = 30 , c = 10 , d = 0;
+		if((a<b)&&(b==c)) {
+			d = a+b+c;
+			System.out.println("sum : "+d);
+		}
+		else
+			System.out.println("false");
+		}
+	}
+
+
