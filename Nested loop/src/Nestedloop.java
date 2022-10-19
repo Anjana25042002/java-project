dc676ee61150b6badf022038ccde4f01c087a6ac
ELCOT <ELCOT@Lenovo>
2022-10-19 14:33:25 +0530

    second commit

---
+++ Nested loop/src/Nestedloop.java
@@ -0,0 +1,16 @@
+
+public class Nestedloop {
+
+	public static void main(String[] args) {
+		int weeks = 3;
+		int days = 7;
+		for(int i = 1; i <= weeks; ++i) {
+			System.out.println("week: " + i);
+			for(int j = 1; j<= days; ++j) {
+				System.out.println(" days: "+ j);
+			}
+		}
+
+	}
+
+}
