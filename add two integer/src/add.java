770fc208e8d10b22bd2d8a09c46c90482109a798
ELCOT <ELCOT@Lenovo>
2022-10-19 10:17:03 +0530

    first commit

---
+++ add two integer/src/add.java
@@ -0,0 +1,13 @@
+
+public class add {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("a+b");
+		int a=10;
+		int b=20;
+		int sum=a+b;
+		System.out.println("the sum is: "+sum);
+	}
+
+}
